3fc1ec5f734508b21166d8e06fbb691c48d2b1ee
Arsen <60201776+c45@users.noreply.github.com>
2023-08-01 15:44:32 +0300

    Add docker integration

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "network" {
 
 module "app" {
   source         = "c45/app/bestrong"
-  version        = "2.0.0"
+  version        = "2.1.0"
   resource_group = azurerm_resource_group.this.name
   location       = azurerm_resource_group.this.location
   plan_name      = "${terraform.workspace}bestrongserviceplan"
@@ -22,9 +22,12 @@ module "app" {
   storage_account_key  = azurerm_storage_account.this.primary_access_key
   fileshare_name       = azurerm_storage_share.fileshare.name
 
-	docker_registry = azurerm_container_registry.this.login_server
-	docker_username = azurerm_container_registry.this.admin_username
-	docker_password = azurerm_container_registry.this.admin_password
+  docker_registry = azurerm_container_registry.this.login_server
+  docker_username = azurerm_container_registry.this.admin_username
+  docker_password = azurerm_container_registry.this.admin_password
+
+  docker_image = "${azurerm_container_registry.this.login_server}/application"
+  docker_tag   = "latest"
 
   env_name = terraform.workspace
 }
